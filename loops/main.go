c937c70af21fc50d7de077cecb9e82031f42944f
auleian <aule.ian@students.mak.ac.ug>
2025-10-07 20:12:42 +0300

    Exercise on loops

---
+++ loops/main.go
@@ -0,0 +1,11 @@
+package main
+
+
+func bulkSend(numMessages int) float64 {
+	totalCost := 0.0
+	for i := 0.0; i < float64(numMessages); i ++ {
+		totalCost += 1.0 + i*0.01
+	}
+
+	return totalCost
+}
