b61db4766633507e253d81cc8323f9a244467588
auleian <aule.ian@students.mak.ac.ug>
2025-10-16 16:50:21 +0300

    final activity in slices

--- sliced/Tagger/main.go
+++ sliced/Tagger/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type sms struct {
 	id      string
 	content string
@@ -7,10 +9,22 @@ type sms struct {
 }
 
 func tagMessages(messages []sms, tagger func(sms) []string) []sms {
-	// ?
+	for i := range messages {
+		messages[i].tags = tagger(messages[i])
+	}
+
+	return messages
 }
 
 func tagger(msg sms) []string {
 	tags := []string{}
-	// ?
+	text := strings.ToLower(msg.content)
+
+	if strings.Contains(text, "urgent"){
+		tags = append(tags, "Urgent")
+	}
+	if strings.Contains(text, "sale") {
+		tags = append(tags, "Promo")
+	}
+	return tags
 }
