e7ec02d580e364d903e44a7e55e35b00c95f89b5
auleian <aule.ian@students.mak.ac.ug>
2025-10-18 00:56:32 +0300

    assignment n mutation

--- maps/lect_2/main.go
+++ maps/lect_2/main.go
@@ -1,7 +1,22 @@
 package main
 
+import "errors"
+
 func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
-	// ?
+	deleted = false
+	person, ok := users[name]
+	if !ok {
+		err := errors.New("not found")
+		return deleted, err
+	}
+	
+	if person.scheduledForDeletion {
+		delete(users, name)
+		deleted = true
+		return deleted, nil
+	}else{
+		return deleted, nil
+	}
 }
 
 type user struct {
