8bd3a736ab55725eea4c9027de8fe7ce80106539
auleian <aule.ian@students.mak.ac.ug>
2025-11-14 20:33:55 +0300

    corrected error

--- DSA/add.go
+++ DSA/add.go
@@ -1,10 +1,14 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+package main
+
+ type ListNode struct {
+     Val int
+     Next *ListNode
+  }
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
     
+}
+
+func main(){
+
 }
