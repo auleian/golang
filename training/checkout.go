bdeaf1936ac66cfb0fce6937ab67f3718758ac24
auleian <aule.ian@students.mak.ac.ug>
2025-11-19 21:19:11 +0300

    solution_1

--- training/checkout.go
+++ training/checkout.go
@@ -2,5 +2,14 @@ package main
 
 func checkoutTotal(subtotal int, hasCoupon bool, couponType string, isVIP bool, isFirstOrder bool, country string) int {
 	// your code here
+	if hasCoupon == true {
+		if couponType == "PERCENT10" {
+			subtotal *= 0.9
+		}else if couponType == "FLAT500" {
+			subtotal -= 500
+		}else{
+			continue
+		}
+	}
 	return 0
 }
