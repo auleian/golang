67961163db7c601f111c48b3a34482fe526220e8
Aule <auleian@icloud.com>
2026-01-02 22:29:32 +0300

    web programming in go

---
+++ web/cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main(){
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("starting server on :5000")
+	err := http.ListenAndServe(":5000", mux)
+	log.Fatal(err)
+}
